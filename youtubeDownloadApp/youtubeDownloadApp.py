b5cd79c3dc8d533758e4609e28a92dd9f96b2f5c
Guilherme Theis <guilhermetheis15@gmail.com>
2018-08-28 17:13:34 +0200

    added mp3 option and conversion

--- youtubeDownloadApp/youtubeDownloadApp.py
+++ youtubeDownloadApp/youtubeDownloadApp.py
@@ -6,6 +6,8 @@
 import os, argparse
 
 from pytube import YouTube
+from pathlib import Path
+from tqdm import tqdm
 
 # Creating the argparse
 
@@ -13,8 +15,10 @@ parser = argparse.ArgumentParser()
 
 # Allows inputs from bash
 
-parser.add_argument ('-url', required =True) # IP Address
-parser.add_argument ('-path', required =True) # IP Address
+parser.add_argument ('-url', required =True) # URL  
+parser.add_argument ('-path', required =True) # Path to save
+parser.add_argument ('-mp3', required=True, type=bool)
+
 args = parser.parse_args() 
 
 # Creating a yt object
@@ -23,9 +27,9 @@ yt = YouTube(args.url)
 
 #print(yt.title) # Verifying if correctly connected with argparse
 
-#Chosing the quality of the video (first always higher quality)
+# Chosing the quality of the video (first always higher quality)
 
-stream = yt.streams.first()
+stream = yt.streams.filter(only_audio=args.mp3).first()
 
 #print(stream) # Veryfing stream
 
@@ -36,4 +40,8 @@ print('Downloading ' + yt.title + " from YouTube url: " + args.url)
 # Downloading the video (best quality)
 
 stream.download(args.path)
+print('File name :'+stream.default_filename) # Verifying if filename works
 
+# Changing extension to mp3
+p = Path('videos/'+stream.default_filename)
+p.rename(p.with_suffix('.mp3'))
