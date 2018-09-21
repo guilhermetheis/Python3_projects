7b07cfbed487245ed65be5071172f4ddd6664d0a
Guilherme Theis <guilhermetheis15@gmail.com>
2018-09-21 14:59:13 +0200

    backup commit

--- youtubeDownloadApp/youtubeDownloadApp.py
+++ youtubeDownloadApp/youtubeDownloadApp.py
@@ -43,32 +43,16 @@ yt = YouTube(args.url)
 
 # Chosing the quality of the video (first always higher quality)
 
-if args.mp3 == False:
-		stream = yt.streams.first() # Taking the best quality for good video
+stream = yt.streams.filter(only_audio=args.mp3).last() # Taking the worst quality to faster download
 
 
-else:
-	print('got here2')
-	stream = yt.streams.filter(only_audio=args.mp3).last() # Taking the worst quality to faster download
-	
 
 #print(stream) # Veryfing stream
 
 # Print to know which video you're downloading
 
-print('Downloading ' + stream.default_filename + " from YouTube url: " + args.url)
+print('Downloading ' + yt.title + " from YouTube url: " + args.url)
 # Downloading the video (best quality)
 
 stream.download(args.path)
-#print('File name :'+stream.default_filename) # Verifying if filename works
-
-if args.mp3 == False: # displaying were it was saved
-
-	print('File ' + p.name() + ' sucefully downloaded into the ' + args.path)
-	
-else: # Changing extension to mp3 if audio
-
-	p = Path('videos/'+stream.default_filename)
-	p.rename(p.with_suffix('.mp3'))
-	os.remove('videos/'+stream.default_filename) #removing old file after conversion
-	print('File ' + p.name() + ' sucefully downloaded into the ' + args.path)
+print('File name :'+stream.default_filename) # Verifying if filename works
