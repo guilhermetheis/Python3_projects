b3832f8e6443b6cc770aa6000a1c67ccb522cb1e
Guilherme Theis <guilhermetheis15@gmail.com>
2019-03-30 23:30:03 +0100

    bug:   File pySnake.py, line 77, in on_execute     pygame.event.pump() pygame.error: video system not initialized

--- Ongoing Projects/pySnake/pySnake.py
+++ Ongoing Projects/pySnake/pySnake.py
@@ -36,38 +36,36 @@ class Player: #creating the player
 
 class GameWindow:
 
-    # simple 800x600 window
     windowWidth = 800
     windowHeight = 600
-
     player = 0
-
-    def __init__(self): #init of the clas
-        pygame.init()
-        self._running = True #private
+ 
+    def __init__(self):
+        self._running = True
         self._display_surf = None
         self._image_surf = None
-        self.player = Player()
-
+        self.player = Player() 
+ 
     def on_init(self):
         pygame.init()
-        self._display_surf = pygame.display.set_mode((self.windowWidth, self.windowHeight)) # creating display
-        pygame.display.set_caption('PySnake '+stringVersion)
+        self._display_surf = pygame.display.set_mode((self.windowWidth,self.windowHeight), pygame.HWSURFACE)
+ 
+        pygame.display.set_caption('Pygame pythonspot.com example')
         self._running = True
         self._image_surf = pygame.image.load("pygame.png").convert()
-
+ 
     def on_event(self, event):
         if event.type == QUIT:
             self._running = False
-    
+ 
     def on_loop(self):
         pass
-    
+ 
     def on_render(self):
-        self._display_surf.fill((0, 0, 0))
-        self._display_surf.blit(self._image_surf, (self.player.x, self.player.y), pygame.HWSURFACE)
+        self._display_surf.fill((0,0,0))
+        self._display_surf.blit(self._image_surf,(self.player.x,self.player.y))
         pygame.display.flip()
-
+ 
     def on_cleanup(self):
         pygame.quit()
  
@@ -94,10 +92,10 @@ class GameWindow:
             if (keys[K_ESCAPE]):
                 self._running = False
  
-        self.on_loop()
-        self.on_render()
-        self.on_cleanup()
- 
+            self.on_loop()
+            self.on_render()
+            self.on_cleanup()
+
 if __name__ == "__main__":
     theWindow = GameWindow()
     theWindow.on_execute()
